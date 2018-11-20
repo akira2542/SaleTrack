9aa90fa816d4e8828371051b2e4d66f1302efbee
akira2542 <akiraakamura@gmail.com>
2018-11-20 20:44:05 +0700

    added UI for Productpage

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
     <div class="insert-panel">
     	<h1>New Product</h1>
     	<form action="includes/createprod.inc.php" method="POST">
-            <div class="inserbox-container">
+            <div class=".insertbox-container">
                  <div class="insertbox" id="pn">Product Name <input type="text" name="productname" required>
                  </div>
-                 <div class="insertbox" id="cp">Capital Price <input type="text" name="capitalprice" required>
+                 <div class="insertbox" id="cp">Capital Price <input type="number" name="capitalprice" required>
                  </div>
-                 <div class="insertbox" id="sp">Sell Price <input type="text" name="sellprice" required>
+                 <div class="insertbox" id="sp">Sell Price <input type="number" name="sellprice" required>
                  </div>                      
            </div>
            <input class="addlist-button" id="submit-bot"type="submit" value="Submit" name="submit">
--- productpage.php
+++ productpage.php
@@ -9,5 +9,38 @@
   	echo "$id";
   }
 	?>
+ <div class="bigcontainer">
+ 	<h1>Fried Chicken Noodle</h1>
+  <div class="product-display-container">
+    <div class="line-1-container">
+ 	 <div class="text-block line-1">COST 20</div>
+ 	 <div class="text-block line-1">PRICE 40</div>
+ 	 <div class="text-block line-1">CHANGE</div>
+
+    </div>
+      <div class="line-2-container">
+ 	 <div class="text-block line-2">TOTAL SOLD<br><span class="big">20</span></div>
+ 	 <div class="text-block line-2">TODAY SOLD<br><span class="big">5</div>
+ 	 <div class="text-block line-2">ADD SALE<br><span class="big">+</div>	
+    </div>
+  </div>
+ </div>
+
+
+
+ <div class="insert-amountpanel">
+ 	<h1>Today's Sale</h1>
+   <div class="insertbox-container">
+	<form action="index.php" method="GET" required>
+		 <div id="dt" class="insertbox">Date
+			<input type="date" name="date">
+		 </div>
+		<div id="am" class="insertbox" required>Amount
+		    <input type="number" name="amount">
+	    </div>   
+   </div>
+     <input class="addlist-button-amount" id="add-bot" type="submit" name="submit" value="Add">	
+	</form>
+   </div>
 </body>
 </html>
